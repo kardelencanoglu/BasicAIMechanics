b7f942349b93dcadd785bd80168877bba76a6edb
Kardelen Canoğlu <kardelen.canoglu7@gmail.com>
2022-12-15 22:52:21 +0300

    Initial commit

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))] //NavMeshAgent için gerekli //komponenti gerekli kýlmak için
+
+public class ClickToMove : MonoBehaviour
+{
+    NavMeshAgent m_Agent;
+    RaycastHit m_HitInfo = new RaycastHit();
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+            {
+                m_Agent.destination = m_HitInfo.point;
+            }
+        }
+    }
+}
